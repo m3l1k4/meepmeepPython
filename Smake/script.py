219e494fe2bfe9fc4cb41801dacf2996278a7367
m3l1k4 <melikaihelas@gmail.com>
2021-01-14 13:51:59 -0800

    main def

--- Smake/script.py
+++ Smake/script.py
@@ -51,3 +51,35 @@ class food(object):
     def draw(self, surface):
         pass
 
+
+
+def genGrid(Surface):
+    for y in range(0,int(GRID_HGHT)):
+        for x in range (0, int(GRID_WDTH)):
+            if ( y + x) % 2 == 0:
+                r = pygame.Rect((x*GRIDSIZE, y*GRIDSIZE), (GRIDSIZE,GRIDSIZE))
+                pygame.draw.rect(surface,(93, 216, 228), r)
+
+            else:
+                r_b = pygame.Rect((x*GRIDSIZE, y*GRIDSIZE),(GRIDSIZE,GRIDSIZE))
+                pygame.draw.rect(surface,(84,194, 205), r_b)
+
+def main():
+    pygame.init()
+
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((SC_WDTH, SC_HGHT),0,32)
+
+    surface = pygame.Surface(screen.get_size())
+    surface = surface.convert()
+
+    genGrid(surface)
+
+    score = 0
+    while (True):
+        clock.tick(10)
+
+        screen.blit(surface, (0,0))
+        pygame.display.update()
+
+main()
